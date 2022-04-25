13230195654f47dc2ec5b3f63c076575a309c62b
mark <markpackham1@gmail.com>
2022-04-25 10:02:54 +0100

    add class_method public count

--- app/models/concerns/visible.rb
+++ app/models/concerns/visible.rb
@@ -1,13 +1,22 @@
 module Visible
-  extend ActiveSupport::Concern
+    extend ActiveSupport::Concern
+  
+    VALID_STATUSES = ['public', 'private', 'archived']
+  
+    included do
+      validates :status, inclusion: { in: VALID_STATUSES }
+    end
+  
+    class methods do
+        def public_count
+          where(status: 'public').count
+        end
+      end
+    
+    end
 
-  VALID_STATUSES = %w[public private archived]
-
-  included do
-    validates :status, inclusion: { in: VALID_STATUSES }
-  end
-
-  def archived?
-    status == 'archived'
-  end
+    def archived?
+      status == 'archived'
+    end
 end
+  
